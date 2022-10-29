12dcd35b85c32865f1bbe2e62bb2053cb4ba4470
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-29 00:16:37 -0600

    Update index.js in backend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongodb from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 import RestaurantsDAO from "./dao/restaurantsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 const port = process.env.PORT || 8000;
 const MongoClient = mongodb.MongoClient;
@@ -16,6 +17,7 @@ MongoClient.connect(
   process.exit(1);
 }).then(async client => {
   await RestaurantsDAO.injectDB(client);
+  await ReviewsDAO.injectDB(client);
   app.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
