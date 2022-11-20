e9ca9954fdd010eb4db32030c96c4d5c161d70ab
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-11-20 14:15:08 -0700

    Update App.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "App.css";
+import Header from "components/Header";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const login = (user = null) => {
+    setUser(user);
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
   return (
     <div>
+      <Router>
+        <Header user={user} userLogout={logout} />
+      </Router>
     </div>
   );
 }
