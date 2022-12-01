010ea743e1ef55117a9295be5d29e00f1bf89354
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-11-30 21:03:05 -0700

    Update RestaurantCard.js in frontend

--- frontend/src/components/RestaurantCard.js
+++ frontend/src/components/RestaurantCard.js
@@ -1,8 +1,48 @@
-function RestaurantCard() {
+import { Link } from "react-router-dom";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import "components/RestaurantCard.css";
+
+function RestaurantCard({ restaurant }) {
+  const address =
+    restaurant.address.building +
+    " " +
+    restaurant.address.street +
+    ", " +
+    restaurant.address.zipcode;
+
   return (
-    <div>
-      
-    </div>
+    <Col lg="6" xl="4" className="restaurantCard">
+      <Card>
+        <Card.Body>
+          <Card.Title>{restaurant.name}</Card.Title>
+          <Card.Text>
+            <strong>Cuisine: </strong>{restaurant.cuisine}
+            <br />
+            <strong>Address: </strong>{address}
+          </Card.Text>
+          <Row className="cardBtns">
+            <Col>
+              <Button variant="outline-primary" className="cardBtn">
+                <Link to={"/restaurants/" + restaurant._id}>
+                  View Reviews
+                </Link>
+              </Button>
+            </Col>
+            <Col>
+              <Button variant="outline-primary" className="cardBtn">
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href={"https://www.google.com/maps/place/" + address}
+                >
+                  View Map
+                </a>
+              </Button>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
 
