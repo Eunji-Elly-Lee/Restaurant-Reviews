1db6e6fc69d6205e04d7ce47cae99e745c6e70b5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-12-06 00:33:33 -0700

    Update App.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 import "App.css";
 import Header from "components/Header";
-import { useState } from "react";
+import Restaurants from "components/Restaurants";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -17,6 +18,9 @@ function App() {
     <div>
       <Router>
         <Header user={user} userLogout={logout} />
+        <Routes>
+          <Route path={`${process.env.PUBLIC_URL}/`} element={<Restaurants />} />
+        </Routes>        
       </Router>
     </div>
   );
