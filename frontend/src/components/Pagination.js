f030d931bde084c8383657d39e2069c1de131464
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-12-07 00:18:48 -0700

    Update Pagination.js in frontend

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -1,7 +1,54 @@
-function Pagination() {
+import usePagination, { DOTS } from "hooks/usePagination";
+import "components/Pagination.css";
+
+function Pagination({ currentPage, totalResults, pageSize, onPageChange }) {
+  const paginationRange =
+    usePagination({ currentPage, totalResults, pageSize });
+
+  const onNext = () => {
+    if (currentPage + 1 < Math.ceil(totalResults / pageSize)) {
+      onPageChange(currentPage + 1);
+    }    
+  };
+
+  const onPrevious = () => {
+    if (currentPage > 0) {
+      onPageChange(currentPage - 1);
+    }    
+  };
+  
   return (
-    <div>
-      
+    <div className="pagination">
+      <ul>
+        <li className="pageBtn" onClick={onPrevious}>
+          <strong>&lt;</strong>
+        </li>
+
+        {paginationRange?.map((pageNumber, index) => {
+          if (pageNumber === DOTS) {
+            return (
+              <li key={index}>&#8230;</li>
+            );
+          }
+
+          return (
+            <li
+              key={index}
+              className={
+                "pageBtn" + 
+                (currentPage + 1 === pageNumber ? " currentPage" : "")
+              }
+              onClick={() => onPageChange(pageNumber - 1)}
+            >
+              {pageNumber}
+            </li>
+          );
+        })}
+
+        <li className="pageBtn" onClick={onNext}>
+          <strong>&gt;</strong>
+        </li>
+      </ul>
     </div>
   );
 }
