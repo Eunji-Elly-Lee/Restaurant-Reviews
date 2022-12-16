06543d6e1d2162f5c3fdc0a16cd4988a23731147
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-12-15 23:05:18 -0700

    Update Header.js in frontend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,24 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "components/Header.css";
 
 function Header({ user, userLogout }) {
   return (
     <Navbar expand="md" variant="dark" bg="primary">
       <Container>
-        <Navbar.Brand href="/">Restaurant Reviews</Navbar.Brand>
+        <Navbar.Brand>Restaurant Reviews</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/" active="true">Restaurants</Nav.Link>
+          <Nav className="nav">
+            <Link to="/">Restaurants</Link>
             {user ? (
-              <Nav.Link href="/" active="true" onClick={userLogout}>
-                Logout - {user.userName}
-              </Nav.Link>
+              <Link to="/"onClick={userLogout}>
+                Logout - {user.name}
+              </Link>
             ) : (
               <>
-                <Nav.Link href="/login" active="true">Login</Nav.Link>
-                <Nav.Link href="/join" active="true">Join</Nav.Link>
+                <Link to="/login">Login</Link>
+                <Link to="/join">Join</Link>
               </>
             )}
           </Nav>
