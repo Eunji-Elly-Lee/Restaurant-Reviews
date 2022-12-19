c6562a2fad57787b5e6679ceef5d0d2d0ec70f6c
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-12-19 13:21:35 -0700

    Update users.controller.js in backend

--- backend/api/users.controller.js
+++ backend/api/users.controller.js
@@ -3,16 +3,16 @@ import UsersDAO from "../dao/usersDAO.js";
 export default class UsersController {
   static async apiAddUser(req, res, next) {
     try {
-      const userId = req.body.userId;
-      const userName = req.body.userName;
-      const password = req.body.password;
+      const userId = req.query.userId;
+      const userName = req.query.userName;
+      const password = req.query.password;
       const UserResponse = await UsersDAO.addUser(
         userId,
         userName,
         password
       );
 
-      res.json({ status: "success" });
+      res.json(UserResponse);
     } catch (e) {
       res.status(500).json({ error: e.message });
     }
