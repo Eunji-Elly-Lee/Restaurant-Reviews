f69f81877064eb9147ef73f1194aea75544e338c
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-01-20 12:16:29 -0700

    Update App.js in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "components/Header";
 import Restaurants from "components/Restaurants";
 import Login from "components/Login";
 import Join from "components/Join";
+import Reviews from "components/Reviews";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -23,8 +24,10 @@ function App() {
         <Header user={user} userLogout={logout} />
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Restaurants />} />
+          <Route path={`${process.env.PUBLIC_URL}/restaurants`} element={<Restaurants />} />
           <Route path={`${process.env.PUBLIC_URL}/login`} element={<Login login={login} />} />
           <Route path={`${process.env.PUBLIC_URL}/join`} element={<Join login={login} />} />
+          <Route path={`${process.env.PUBLIC_URL}/restaurants/:id`} element={<Reviews user={user} />} />
         </Routes>        
       </Router>
     </div>
