3c23671955fb7a833933af7e8ef06db9e78420ae
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-01-24 00:30:46 -0700

    Update ReviewCard.js in frontend

--- frontend/src/components/ReviewCard.js
+++ frontend/src/components/ReviewCard.js
@@ -1,8 +1,48 @@
-function ReviewCard() {
+import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import "components/ReviewCard.css";
+
+function ReviewCard({ user, id, review, index, onDelete }) {
+  const date = review.date.substring(10, 0);
+
   return (
-    <div>
-      
-    </div>
+    <Card key={review._id} className="reviewCard">
+      <Card.Body>
+        <Card.Text>
+          <strong>User: </strong>
+          {review.name}
+          <br />
+          <strong>Date: </strong>
+          {date}
+          <br />
+          <strong>Review: </strong>
+          {review.text}
+
+          {user && user.user_id === review.user_id && (
+            <>
+              <br />
+              <Button
+                variant="outline-primary"
+                className="deleteReviewBtn"
+                onClick={() => onDelete(review._id, index)}
+              >
+                Delete
+              </Button>
+              <Button variant="outline-primary" className="editReviewBtn">
+                <Link
+                  to={"/restaurants/" + id + "/review"}
+                  state={{
+                    currentReview: review,
+                  }}
+                >
+                  Edit
+                </Link>
+              </Button>
+            </>
+          )}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
